a6173fdfe87692f861b81de7aed58b140bf02bd1
Jonnelin Leonardo <jonnelin.leonardo@gmail.com>
2022-08-15 21:53:14 -0400

    Setup initial server

---
+++ server.js
@@ -0,0 +1,13 @@
+
+const express = require('express');
+const morgan = require('morgan');
+const fs = require('fs/promises');
+let dotenv = require('dotenv');
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+    console.log(`[start] Listening on port ${PORT}...`);
+});
+
