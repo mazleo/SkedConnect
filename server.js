e0f64afcc64091a0def0d09171291903f1fa17d1
Jonnelin Leonardo <jonnelin.leonardo@gmail.com>
2022-08-15 21:55:55 -0400

    Configure access to environment variables

--- server.js
+++ server.js
@@ -7,9 +7,36 @@ let dotenv = require('dotenv');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+app.use(async (req, res, next) => {
+    await configureDotEnv();
+    next();
+});
+
 app.use(morgan('dev'));
 
 app.listen(PORT, () => {
     console.log(`[start] Listening on port ${PORT}...`);
 });
 
+async function configureDotEnv() {
+    const envExists = await isEnvExists();
+    if (envExists) {
+        dotenv.config({path: '.env'});
+    }
+    else {
+        dotenv.config({path: 'app.yaml'})
+    }
+}
+
+async function isEnvExists() {
+    let envExists = true;
+    try {
+        await fs.access('.env');
+    }
+    catch(error) {
+        console.error(error);
+        envExists = false;
+    }
+
+    return envExists;
+}
